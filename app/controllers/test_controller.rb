ddb6305da50995f5d4f9b7c4a2acf89d368ac5ed
XAINI <shanzhenqiang0814@163.com>
2017-02-26 14:01:36 +0800

    项目经理中的项目列表

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,9 @@
 class TestController < ApplicationController
   def index
+    @num_ary = [60,21,43,42,15,60,70,81,29,100,11,56,33,22,15,77,19,100,30,20,60,22,0,98,25,100,27,28,29,0]
+  end
+
+  def new
     
   end
 end
